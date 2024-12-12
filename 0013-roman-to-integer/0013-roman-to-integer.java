403a30bc4f544d21dccf1c9e7f6e06343a20a080
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2024-12-13 00:24:38 +0530

    Time: 2 ms (100%), Space: 44.8 MB (47.34%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,29 +1,29 @@
+
 class Solution {
     public int romanToInt(String s) {
-        //xc
-        int result=0;
-        int num=0;
-        for(int i=s.length()-1;i>=0;i--){
-            switch(s.charAt(i)){
-                case 'I': num=1;break;
-                case 'V': num=5;break;
-                case 'X': num=10;break;
-                case 'L': num=50;break;
-                case 'C': num=100;break;
-                case 'D': num=500;break;
-                case 'M': num=1000;break;
-                
+        // xc
+        int result = 0;
+        int num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+                case 'I' -> num = 1;
+                case 'V' -> num = 5;
+                case 'X' -> num = 10;
+                case 'L' -> num = 50;
+                case 'C' -> num = 100;
+                case 'D' -> num = 500;
+                case 'M' -> num = 1000;
 
             }
-            if(num*3<result){
-                result-=num;
+            if (num * 3 < result) {
+                result -= num;
 
-            }else{
-                result+=num;
+            } else {
+                result += num;
             }
 
         }
         return result;
-        
+
     }
 }
